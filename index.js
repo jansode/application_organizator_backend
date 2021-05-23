eb52ea85e42654606bed539fcdbb92f977c424ca
Jan Söderberg <jan.soderberg24@gmail.com>
2021-05-23 12:40:58 +0300

    Change port to 0.0.0.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./app')
 require('dotenv').config()
 
 PORT = process.env.PORT 
-HOST = 'localhost'
+HOST = '0.0.0.0'
 app.listen(PORT, HOST, () => {
     console.log('Server running');
 });

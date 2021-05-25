692f2f233e128cd231794fab8bc0ed7dc90a9811
Jan Söderberg <jan.soderberg24@gmail.com>
2021-05-25 14:05:39 +0300

    Fix some routing issues.

--- app.js
+++ app.js
@@ -23,11 +23,9 @@ app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-/*
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
-*/
 
 app.use('/api/users', usersRouter)
 app.use('/api/applications', applicationsRouter)
